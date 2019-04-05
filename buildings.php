132c818d1681c47518f88ed43b08f7527f9faa60
Matt Hunt <hmatt33@gmail.com>
2019-04-05 02:31:17 -0400

    added buildings page with placeholder text

---
+++ buildings.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<!--Placeholder for now, meant to test login-->
+	<h1>You Logged in</h1>
+
+	<footer>
+		<?php include('footer.php'); ?>
+	</footer>
+</body>
+</html>

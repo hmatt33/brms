b5d1b03ac6bc2c565fc9527ac9ac0c135f7a7078
Matt Hunt <hmatt33@gmail.com>
2019-04-05 02:33:15 -0400

    creates mysql database connection

---
+++ mysqli_connect.php
@@ -0,0 +1,13 @@
+<?php 
+// This creates a connection to the database and to MySQL, 
+// Set the access details as constants:
+DEFINE ('DB_HOST', '127.0.0.1');
+DEFINE ('DB_USER', 'root');
+DEFINE ('DB_PASSWORD', 'seedubuntu');
+DEFINE ('DB_NAME', 'brms');
+
+// Make the connection:
+$dbcon = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
+
+// Set the encoding...
+mysqli_set_charset($dbcon, 'utf8');

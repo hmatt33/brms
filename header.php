99fda22ad7154fe51920d021618c2f21b311c86d
Matt Hunt <hmatt33@gmail.com>
2019-04-06 19:37:28 -0400

    updated to include admin page link

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <div id="header">
         <h1>Building and Resident Management System</h1>
         <?php
-        //displays person who is logged in
+            //displays person who is logged in
             echo '<p>Hello ';
             if(isset($_SESSION['FirstName'])) {
                 echo "{$_SESSION['FirstName']}";
@@ -17,7 +17,8 @@
         <div id="header-nav">
             <ul>
                 <li><a href="buildings.php">Home</a></li>
-                <li><a href="faq.html">FAQ</a></li>
+		<li><a href="faq.html">FAQ</a></li>
+		<li><a href="admin_page.php">Admin Page</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>

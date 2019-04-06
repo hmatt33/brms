b2838d54df68ac1f797d229b9b3bc7f0feaa31a9
Matt Hunt <47961957+hmatt33@users.noreply.github.com>
2019-04-06 01:13:37 -0400

    updated header, added nav links

---
+++ header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+    <div id="header">
+        <h1>Building and Resident Management System</h1>
+        <?php
+        //displays person who is logged in
+            echo '<p>Hello ';
+            if(isset($_SESSION['FirstName'])) {
+                echo "{$_SESSION['FirstName']}";
+            }
+            echo '</p>';
+        ?>
+        <div id="header-nav">
+            <ul>
+                <li><a href="buildings.php">Home</a></li>
+                <li><a href="faq.html">FAQ</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>

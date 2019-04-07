3b4eab2525a0e5be1a8fda1b31e73d5dca8eb49b
milkshakegirl <39037588+milkshakegirl@users.noreply.github.com>
2019-04-07 13:57:42 -0400

    Revert "fixed some typos and display formatting. add_buildings.php implemented"
    This reverts commit 51723366062c13e9c71e02d897599170680eb082.

--- delete_building.php
+++ delete_building.php
@@ -57,6 +57,7 @@
             <input id="submit-no" type="submit" name="sure" value="No">
             <input type="hidden" name="BuildingID" value="' . $id . '">
             </form>';
+
         } else { // Not a valid user ID.
             echo '<p class="error">This page has been accessed in error.</p>';
             echo '<p>&nbsp;</p>';
@@ -64,6 +65,7 @@
     } // End of the main submission conditional.
     mysqli_close($dbcon );
     echo '<p>&nbsp;</p>';
+
     include ('footer.php');
 ?>
 </div>

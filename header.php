e7ffcdc9643ea0a90a0a4051d5e1c6ef051dbca2
Matt Hunt <hmatt33@gmail.com>
2019-04-07 00:07:37 -0400

    added change password link

--- header.php
+++ header.php
@@ -17,8 +17,9 @@
         <div id="header-nav">
             <ul>
                 <li><a href="buildings.php">Home</a></li>
-		<li><a href="faq.html">FAQ</a></li>
-		<li><a href="admin_page.php">Admin Page</a></li>
+                <li><a href="faq.html">FAQ</a></li>
+                <li><a href="admin_page.php">Admin Page</a></li>
+                <li><a href="password.html">Change Password</a></li>
                 <li><a href="logout.php">Logout</a></li>
             </ul>
         </div>

f1618cdcc49d8e641079fd4e590d5bf7350feac9
Matt Hunt <hmatt33@gmail.com>
2019-04-11 19:41:58 -0400

    fixed unclosed tags, cleaned up structure

--- header.php
+++ header.php
@@ -7,35 +7,28 @@
 </head>
 <body>
     <div id="header">
-        <div id="header-nav">
-	    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  		<a class="navbar-brand" href="buildings.php">Home</a>
-  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="faq.html">FAQ</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="admin_page.php">
-          Admin
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="password.html">
-          Change Password
-        </a>
-      </li>
-	<li class="nav-item">
-        <a class="nav-link" href="logout.php">
-          Logout
-        </a>
-      </li>
-    </ul>
-        </div>
+      <div id="header-nav">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+          <a class="navbar-brand" href="buildings.php">Home</a>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="faq.html">FAQ</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="admin_page.php">Admin</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="password.html">Change Password</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">Logout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
     </div>
+  </div>
 <h1 class="text-center font-weight-bold">Building and Resident Management System</h1>
 </body>
 </html>
